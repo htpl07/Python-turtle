a2161a200bc554ccc9b5865b7ca3035d88f98492
htpl07 <52022479+htpl07@users.noreply.github.com>
2019-06-29 14:21:16 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,9 @@
+import turtle as tt
+
+a1=tt.Turtle()
+
+for x in range(50):
+    a1.forward(50)
+    a1.right(114)
+
+tt.done()
---
+++ p6.py
@@ -0,0 +1,14 @@
+import turtle as tt
+c1=tt.Turtle()
+c1.pencolor("pink")
+for i in range(50):
+    c1.forward(50)
+    c1.left(123)
+
+c1.pencolor("yellow")
+for i in range(50):
+    c1.forward(100)
+    c1.left(123)
+
+    
+tt.done()
